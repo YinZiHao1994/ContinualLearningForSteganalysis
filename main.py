258a391ec9bd0f991d330aa25170da4a365b6032
Yin <1035909389@qq.com>
2021-06-10 20:12:59 +0800

    初始化，整理了一些代码

---
+++ main.py
@@ -0,0 +1,414 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import argparse
+import numpy as np
+import pandas as pd
+import cv2
+from pathlib import Path
+import copy
+import logging
+import random
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import time
+from glob import glob
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torch.nn.functional as F
+from torch.utils.data.sampler import SubsetRandomSampler
+from PIL import Image
+# 30 SRM filtes
+# from srm_filter_kernel import all_normalized_hpf_list
+# Global covariance pooling
+# from MPNCOV import *  # MPNCOV
+from net import SRNet
+from enum import Enum
+
+BATCH_SIZE = 32
+EPOCHS = 200
+LR = 0.01
+WEIGHT_DECAY = 5e-4
+TRAIN_PRINT_FREQUENCY = 100
+EVAL_PRINT_FREQUENCY = 1
+STETSIZE = 10
+scheduler_gama = 0.4
+# DECAY_EPOCH = [30, 60, 90, 140, 200, 250, 300, 350]
+# DECAY_EPOCH = [20, 60, 90, 120, 150, 170, 190]
+DECAY_EPOCH = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+OUTPUT_PATH = '/data/log'
+DATASET_DIR = r'D:\Work\dataset\steganalysis\BOSSBase'
+
+
+class SteganographyEnum(Enum):
+    HILL = 1
+    SUNI = 2
+    UTGAN = 3
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def train(model, device, train_loader, optimizer, epoch):
+    losses = AverageMeter()
+    model.train()
+    train_loss = 0
+    train_correct = 0
+    total = 0
+
+    for i, sample in enumerate(train_loader):
+
+        data, label = sample['data'], sample['label']
+        shape = list(data.size())
+        data = data.reshape(shape[0] * shape[1], *shape[2:])
+        label = label.reshape(-1)
+        ##shuffle##
+        idx = torch.randperm(shape[0])
+        data = data[idx]
+        label = label[idx]
+
+        data, label = data.to(device), label.to(device)
+
+        optimizer.zero_grad()
+        output = model(data)  # FP
+        criterion = nn.CrossEntropyLoss()
+        loss = criterion(output, label)
+        # losses.update(loss.item(), data.size(0))
+        loss.backward()  # BP
+        optimizer.step()
+        train_loss += loss.item()
+        prediction = torch.max(output, 1)  # second param "1" represents the dimension to be reduced
+        total += label.size(0)
+        train_correct += np.sum(prediction[1].cpu().numpy() == label.cpu().numpy())
+
+        if i % TRAIN_PRINT_FREQUENCY == 0:
+            logging.info('Epoch: [{0}][{1}/{2}]\t'
+                         'Acc {acc:.4f}\t'
+                         'Loss {loss:.4f} \t'
+                         .format(epoch, i, len(train_loader), acc=100. * train_correct / total,
+                                 loss=train_loss / (i + 1)))
+
+
+def evaluate(model, device, eval_loader, epoch, optimizer, best_acc, params_path):
+    model.eval()
+    test_loss = 0.0
+    correct = 0.0
+    total = 0
+    batch_num = 0
+
+    with torch.no_grad():
+        for i, sample in enumerate(eval_loader):
+            data, label = sample['data'], sample['label']
+            shape = list(data.size())
+            data = data.reshape(shape[0] * shape[1], *shape[2:])
+            label = label.reshape(-1)
+            ##shuffle##
+            idx = torch.randperm(shape[0])
+            # data = data[idx]
+            # label = label[idx]
+
+            data, label = data.to(device), label.to(device)
+            output = model(data)
+            criterion = nn.CrossEntropyLoss()
+            loss = criterion(output, label)
+            test_loss += loss.item()
+            batch_num += 1
+
+            pred = output.max(1, keepdim=True)[1]
+            total += label.size(0)
+            correct += pred.eq(label.view_as(pred)).sum().item()
+
+    # accuracy = correct / (len(eval_loader.dataset) * 2)
+    accuracy = 100. * correct / total
+
+    if accuracy > best_acc and epoch > 40:
+        best_acc = accuracy
+        all_state = {
+            'original_state': model.state_dict(),
+            'optimizer_state': optimizer.state_dict(),
+            'epoch': epoch
+        }
+        torch.save(all_state, params_path)
+
+    logging.info('-' * 8)
+    logging.info('Test loss: {:.4f}'.format(test_loss / batch_num))
+    logging.info('Eval accuracy: {:.4f}'.format(accuracy))
+    logging.info('Best accuracy:{:.4f}'.format(best_acc))
+    logging.info('-' * 8)
+    return best_acc, test_loss / batch_num
+
+
+# Initialization
+def initWeights(module):
+    if type(module) == nn.Conv2d:
+        if module.weight.requires_grad:
+            nn.init.kaiming_normal_(module.weight.data, mode='fan_in', nonlinearity='relu')
+
+    if type(module) == nn.Linear:
+        nn.init.normal_(module.weight.data, mean=0, std=0.01)
+        nn.init.constant_(module.bias.data, val=0)
+
+
+# Data augmentation
+class AugData():
+    def __call__(self, sample):
+        data, label = sample['data'], sample['label']
+
+        # Rotation
+        rot = random.randint(0, 3)
+        data = np.rot90(data, rot, axes=[1, 2]).copy()
+
+        # Mirroring
+        if random.random() < 0.5:
+            data = np.flip(data, axis=2).copy()
+
+        new_sample = {'data': data, 'label': label}
+
+        return new_sample
+
+
+class ToTensor():
+    def __call__(self, sample):
+        data, label = sample['data'], sample['label']
+
+        data = np.expand_dims(data, axis=1)
+        data = data.astype(np.float32)
+        # data = data / 255.0
+
+        new_sample = {
+            'data': torch.from_numpy(data),
+            'label': torch.from_numpy(label).long(),
+        }
+
+        return new_sample
+
+
+'''
+class MyDataset(Dataset):
+    def __init__(self, DATASET_DIR, partition, transform=None):
+        random.seed(1234)
+
+        self.transform = transform
+
+        self.cover_dir = DATASET_DIR + '/cover'
+        self.stego_dir = DATASET_DIR + '/stego/' + Model_NAME
+
+        self.covers_list = [x.split('/')[-1] for x in glob(self.cover_dir + '/*')]
+        random.shuffle(self.covers_list)
+        if (partition == 0):
+            self.cover_list = self.covers_list[:4000]
+        if (partition == 1):
+            self.cover_list = self.covers_list[4000:5000]
+        if (partition == 2):
+            self.cover_list = self.covers_list[5000:10000]
+        assert len(self.covers_list) != 0, "cover_dir is empty"
+
+    def __len__(self):
+        return len(self.cover_list)
+
+    def __getitem__(self, idx):
+        file_index = int(idx)
+
+        cover_path = os.path.join(self.cover_dir, self.cover_list[file_index])
+        stego_path = os.path.join(self.stego_dir, self.cover_list[file_index])
+
+        cover_data = cv2.imread(cover_path, -1)
+        stego_data = cv2.imread(stego_path, -1)
+
+        data = np.stack([cover_data, stego_data])
+        label = np.array([0, 1], dtype='int32')
+
+        sample = {'data': data, 'label': label}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+'''
+
+
+class MyDataset(Dataset):
+    def __init__(self, dataset_dir, steganography_enum, transform=None):
+        self.transform = transform
+
+        self.cover_dir = dataset_dir + '/BOSSBase_256'
+        # self.stego_dir = DATASET_DIR + '/stego_suniward04'
+        self.stego_dir = dataset_dir + '/BOSSBase_256_' + steganography_enum.name + '04'
+
+        self.cover_list = [x.split('/')[-1] for x in glob(self.cover_dir + '/*')]
+        assert len(self.cover_list) != 0, "cover_dir is empty"
+
+    def __len__(self):
+        return len(self.cover_list)
+
+    def __getitem__(self, idx):
+        file_index = int(idx)
+
+        cover_path = os.path.join(self.cover_dir, self.cover_list[file_index])
+        stego_path = os.path.join(self.stego_dir, self.cover_list[file_index])
+
+        # cover_data = cv2.imread(cover_path, -1)
+        # stego_data = cv2.imread(stego_path, -1)
+        cover_data = Image.open(cover_path)  # .convert('RGB')
+        stego_data = Image.open(stego_path)  # .convert('RGB')
+        # cover_data = np.array(cover_data)
+        # stego_data = np.array(stego_data)
+
+        # data = np.stack([cover_data, stego_data], ).transpose((0, 3, 1, 2))
+        # print(np.array(cover_data).shape)
+        # data_ = Image.fromarray(data).convert('RGB')
+        label = np.array([0, 1], dtype='uint8')
+        label = torch.from_numpy(label).long()
+        # print(type(data) )
+        # print(type(label) )
+
+        # sample = {'data': data, 'label': label}
+        # print(type(Image.fromarray(data)) )
+
+        if self.transform:
+            cover_data = self.transform(cover_data)
+            stego_data = self.transform(stego_data)
+            # print('cover_data',cover_data.shape)
+        # data = torch.cat((cover_data, stego_data), 0)
+        data = torch.stack((cover_data, stego_data))
+        # print('data', data.shape)
+
+        sample = {'data': data, 'label': label}
+        return sample
+
+
+def setLogger(log_path, mode='a'):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path, mode=mode)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s', '%Y-%m-%d %H:%M:%S'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(message)s'))
+        logger.addHandler(stream_handler)
+
+
+def main(steganography_enum):
+    device = torch.device("cpu")
+    use_gpu = torch.cuda.is_available()
+    if use_gpu:
+        device = torch.device("cuda")
+    kwargs = {'num_workers': 1, 'pin_memory': True}
+
+    train_transform = transforms.Compose([
+        AugData(),
+        ToTensor()
+    ])
+
+    eval_transform = transforms.Compose([
+        ToTensor()
+    ])
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # Log files
+    # params_name = 'SRNET_model_params_boss_256_HILL04.pt'
+    params_name = 'SRNET_model_params_boss_256_' + steganography_enum.name + '04.pt'
+    # log_name = 'SRNET_params_boss_256_HILL04.log'
+    log_name = 'SRNET_params_boss_256_' + steganography_enum.name + '04.log'
+
+    params_path = os.path.join(OUTPUT_PATH, params_name)
+    log_path = os.path.join(OUTPUT_PATH, log_name)
+
+    setLogger(log_path, mode='w')
+
+    # Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
+    data = MyDataset(dataset_dir=DATASET_DIR, steganography_enum=steganography_enum, transform=transform)
+    test_size = 0.2
+    valid_size = 0.1
+    num_data = len(data)
+    indices_data = list(range(num_data))
+
+    np.random.shuffle(indices_data)
+
+    split_tt = int(np.floor(test_size * num_data))
+    train_idx, test_idx = indices_data[split_tt:], indices_data[:split_tt]
+    # For Valid
+    num_train = len(train_idx)
+    indices_train = list(range(num_train))
+    np.random.shuffle(indices_train)
+    split_tv = int(np.floor(valid_size * num_train))
+    train_new_idx, valid_idx = indices_train[split_tv:], indices_train[:split_tv]
+
+    train_sampler = SubsetRandomSampler(train_new_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+    test_sampler = SubsetRandomSampler(test_idx)
+
+    train_loader = DataLoader(data, batch_size=BATCH_SIZE, sampler=train_sampler, )
+    valid_loader = DataLoader(data, batch_size=BATCH_SIZE, sampler=valid_sampler, )
+    test_loader = DataLoader(data, batch_size=BATCH_SIZE, sampler=test_sampler, )
+
+    model = SRNet().to(device)
+    model.apply(initWeights)
+    params = model.parameters()
+
+    params_wd, params_rest = [], []
+    for param_item in params:
+        if param_item.requires_grad:
+            (params_wd if param_item.dim() != 1 else params_rest).append(param_item)
+
+    param_groups = [{'params': params_wd, 'weight_decay': WEIGHT_DECAY},
+                    {'params': params_rest}]
+
+    optimizer = optim.SGD(param_groups, lr=LR, momentum=0.9, weight_decay=0.0005)
+
+    startEpoch = 1
+    # scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=DECAY_EPOCH, gamma=scheduler_gama)
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
+    # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.9)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=STETSIZE, gamma=scheduler_gama)
+    best_acc = 0.0
+
+    for epoch in range(startEpoch, EPOCHS + 1):
+        scheduler.step()
+        train(model, device, train_loader, optimizer, epoch)
+        if epoch % EVAL_PRINT_FREQUENCY == 0:
+            best_acc, test_loss = evaluate(model, device, valid_loader, epoch, optimizer, best_acc, params_path)
+        print('current lr: ', optimizer.state_dict()['param_groups'][0]['lr'])
+        # scheduler.step(test_loss)
+    logging.info('\nTest set accuracy: \n')
+
+    # Load best network parmater to test
+    all_state = torch.load(params_path)
+    original_state = all_state['original_state']
+    optimizer_state = all_state['optimizer_state']
+    model.load_state_dict(original_state)
+    optimizer.load_state_dict(optimizer_state)
+
+    evaluate(model, device, test_loader, EPOCHS, optimizer, best_acc, params_path)
+
+
+if __name__ == '__main__':
+    # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    main(SteganographyEnum.HILL)
---
+++ net.py
@@ -0,0 +1,342 @@
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torch.nn.functional as F
+from torch.utils.data.sampler import SubsetRandomSampler
+from PIL import Image
+# 30 SRM filtes
+from srm_filter_kernel import all_normalized_hpf_list
+# Global covariance pooling
+from MPNCOV import *  # MPNCOV
+
+BATCH_SIZE = 32
+EPOCHS = 200
+LR = 0.01
+WEIGHT_DECAY = 5e-4
+TRAIN_PRINT_FREQUENCY = 100
+EVAL_PRINT_FREQUENCY = 1
+STETSIZE = 10
+scheduler_gama = 0.4
+# DECAY_EPOCH = [30, 60, 90, 140, 200, 250, 300, 350]
+# DECAY_EPOCH = [20, 60, 90, 120, 150, 170, 190]
+DECAY_EPOCH = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
+OUTPUT_PATH = '/data/cuiqi/steganalysis_with_CNN_Yedroudj-Net-master/pytorch_version/log'
+
+
+# Truncation operation
+class TLU(nn.Module):
+    def __init__(self, threshold):
+        super(TLU, self).__init__()
+        self.threshold = threshold
+
+    def forward(self, input):
+        output = torch.clamp(input, min=-self.threshold, max=self.threshold)
+        return output
+
+
+# Pre-processing Module
+class HPF(nn.Module):
+    def __init__(self):
+        super(HPF, self).__init__()
+        # Load 30 SRM Filters
+        all_hpf_list_5x5 = []
+        for hpf_item in all_normalized_hpf_list:
+            if hpf_item.shape[0] == 3:
+                hpf_item = np.pad(hpf_item, pad_width=((1, 1), (1, 1)), mode='constant')
+            all_hpf_list_5x5.append(hpf_item)
+        hpf_weight = nn.Parameter(torch.Tensor(all_hpf_list_5x5).view(30, 1, 5, 5), requires_grad=False)
+        self.hpf = nn.Conv2d(1, 30, kernel_size=5, padding=2, bias=False)
+        self.hpf.weight = hpf_weight
+        # Truncation, threshold = 3
+        self.tlu = TLU(3.0)
+
+    def forward(self, input):
+
+        output = self.hpf(input)
+        output = self.tlu(output)
+        return output
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.group1 = HPF()
+
+        self.group2 = nn.Sequential(
+            nn.Conv2d(30, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.AvgPool2d(kernel_size=3, padding=1, stride=2)
+        )
+
+        self.group3 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.AvgPool2d(kernel_size=3, padding=1, stride=2)
+        )
+
+        self.group4 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+
+            nn.AvgPool2d(kernel_size=3, padding=1, stride=2)
+        )
+
+        self.group5 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+
+        )
+
+        self.fc1 = nn.Linear(int(256 * (256 + 1) / 2), 2)
+
+    def forward(self, input):
+        output = input
+
+        output = self.group1(output)
+        output = self.group2(output)
+        output = self.group3(output)
+        output = self.group4(output)
+        output = self.group5(output)
+
+        # Global covariance pooling
+        output = CovpoolLayer(output)
+        output = SqrtmLayer(output, 5)
+        output = TriuvecLayer(output)
+
+        output = output.view(output.size(0), -1)
+        output = self.fc1(output)
+
+        return output
+
+
+class SRNet(nn.Module):
+    def __init__(self):
+        super(SRNet, self).__init__()
+        # Layer 1
+        self.layer1 = nn.Conv2d(in_channels=1, out_channels=64,
+                                kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        # Layer 2
+        self.layer2 = nn.Conv2d(in_channels=64, out_channels=16,
+                                kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(16)
+        # Layer 3
+        self.layer31 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn31 = nn.BatchNorm2d(16)
+        self.layer32 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn32 = nn.BatchNorm2d(16)
+        # Layer 4
+        self.layer41 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn41 = nn.BatchNorm2d(16)
+        self.layer42 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn42 = nn.BatchNorm2d(16)
+        # Layer 5
+        self.layer51 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn51 = nn.BatchNorm2d(16)
+        self.layer52 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn52 = nn.BatchNorm2d(16)
+        # Layer 6
+        self.layer61 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn61 = nn.BatchNorm2d(16)
+        self.layer62 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn62 = nn.BatchNorm2d(16)
+        # Layer 7
+        self.layer71 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn71 = nn.BatchNorm2d(16)
+        self.layer72 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn72 = nn.BatchNorm2d(16)
+        # Layer 8
+        self.layer81 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=1, stride=2, padding=0, bias=False)
+        self.bn81 = nn.BatchNorm2d(16)
+        self.layer82 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn82 = nn.BatchNorm2d(16)
+        self.layer83 = nn.Conv2d(in_channels=16, out_channels=16,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn83 = nn.BatchNorm2d(16)
+        self.pool1 = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+        # Layer 9
+        self.layer91 = nn.Conv2d(in_channels=16, out_channels=64,
+                                 kernel_size=1, stride=2, padding=0, bias=False)
+        self.bn91 = nn.BatchNorm2d(64)
+        self.layer92 = nn.Conv2d(in_channels=16, out_channels=64,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn92 = nn.BatchNorm2d(64)
+        self.layer93 = nn.Conv2d(in_channels=64, out_channels=64,
+                                 kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn93 = nn.BatchNorm2d(64)
+        self.pool2 = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+        # Layer 10
+        self.layer101 = nn.Conv2d(in_channels=64, out_channels=128,
+                                  kernel_size=1, stride=2, padding=0, bias=False)
+        self.bn101 = nn.BatchNorm2d(128)
+        self.layer102 = nn.Conv2d(in_channels=64, out_channels=128,
+                                  kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn102 = nn.BatchNorm2d(128)
+        self.layer103 = nn.Conv2d(in_channels=128, out_channels=128,
+                                  kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn103 = nn.BatchNorm2d(128)
+        self.pool3 = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+        # Layer 11
+        self.layer111 = nn.Conv2d(in_channels=128, out_channels=256,
+                                  kernel_size=1, stride=2, padding=0, bias=False)
+        self.bn111 = nn.BatchNorm2d(256)
+        self.layer112 = nn.Conv2d(in_channels=128, out_channels=256,
+                                  kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn112 = nn.BatchNorm2d(256)
+        self.layer113 = nn.Conv2d(in_channels=256, out_channels=256,
+                                  kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn113 = nn.BatchNorm2d(256)
+        self.pool3 = nn.AvgPool2d(kernel_size=3, stride=2, padding=1)
+
+        # Layer 12
+        self.layer121 = nn.Conv2d(in_channels=256, out_channels=512,
+                                  kernel_size=3, stride=2, padding=0, bias=False)
+        self.bn121 = nn.BatchNorm2d(512)
+        self.layer122 = nn.Conv2d(in_channels=512, out_channels=512,
+                                  kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn122 = nn.BatchNorm2d(512)
+        # avgp = torch.mean() in forward before fc
+        # Fully Connected layer
+        self.fc = nn.Linear(512 * 1 * 1, 2)
+        self.fc1 = nn.Linear(int(512 * (512 + 1) / 2), 2)
+
+    def forward(self, inputs):
+        # Layer 1
+        conv = self.layer1(inputs)
+        actv = F.relu(self.bn1(conv))
+        # Layer 2
+        conv = self.layer2(actv)
+        actv = F.relu(self.bn2(conv))
+        # Layer 3
+        conv1 = self.layer31(actv)
+        actv1 = F.relu(self.bn31(conv1))
+        conv2 = self.layer32(actv1)
+        bn = self.bn32(conv2)
+        res = torch.add(actv, bn)
+        # Layer 4
+        conv1 = self.layer41(res)
+        actv1 = F.relu(self.bn41(conv1))
+        conv2 = self.layer42(actv1)
+        bn = self.bn42(conv2)
+        res = torch.add(res, bn)
+        # Layer 5
+        conv1 = self.layer51(res)
+        actv1 = F.relu(self.bn51(conv1))
+        conv2 = self.layer52(actv1)
+        bn = self.bn52(conv2)
+        res = torch.add(res, bn)
+        # Layer 6
+        conv1 = self.layer61(res)
+        actv1 = F.relu(self.bn61(conv1))
+        conv2 = self.layer62(actv1)
+        bn = self.bn62(conv2)
+        res = torch.add(res, bn)
+        # Layer 7
+        conv1 = self.layer71(res)
+        actv1 = F.relu(self.bn71(conv1))
+        conv2 = self.layer72(actv1)
+        bn = self.bn72(conv2)
+        res = torch.add(res, bn)
+        # Layer 8
+        convs = self.layer81(res)
+        convs = self.bn81(convs)
+        conv1 = self.layer82(res)
+        actv1 = F.relu(self.bn82(conv1))
+        conv2 = self.layer83(actv1)
+        bn = self.bn83(conv2)
+        pool = self.pool1(bn)
+        res = torch.add(convs, pool)
+        # Layer 9
+        convs = self.layer91(res)
+        convs = self.bn91(convs)
+        conv1 = self.layer92(res)
+        actv1 = F.relu(self.bn92(conv1))
+        conv2 = self.layer93(actv1)
+        bn = self.bn93(conv2)
+        pool = self.pool2(bn)
+        res = torch.add(convs, pool)
+        # Layer 10
+        convs = self.layer101(res)
+        convs = self.bn101(convs)
+        conv1 = self.layer102(res)
+        actv1 = F.relu(self.bn102(conv1))
+        conv2 = self.layer103(actv1)
+        bn = self.bn103(conv2)
+        pool = self.pool1(bn)
+        res = torch.add(convs, pool)
+        # Layer 11
+        convs = self.layer111(res)
+        convs = self.bn111(convs)
+        conv1 = self.layer112(res)
+        actv1 = F.relu(self.bn112(conv1))
+        conv2 = self.layer113(actv1)
+        bn = self.bn113(conv2)
+        pool = self.pool1(bn)
+        res = torch.add(convs, pool)
+        # Layer 12
+        conv1 = self.layer121(res)
+        actv1 = F.relu(self.bn121(conv1))
+        conv2 = self.layer122(actv1)
+        bn = self.bn122(conv2)
+        # print(bn.shape)
+        # avgp = torch.mean(bn, dim=(2,3), keepdim=True)
+        # avgp = torch.mean(bn, dim=2, keepdim=True)
+        # avgp = torch.mean(bn, dim=3, keepdim=True)
+        # avgp = torch.nn.functional.adaptive_avg_pool2d(bn,(1,1))
+        # print(avgp.shape)
+        # flatten = avgp.view(avgp.size(0), -1)
+        # print(flatten.shape)
+        output = CovpoolLayer(bn)
+        output = SqrtmLayer(output, 5)
+        output = TriuvecLayer(output)
+        flatten = output.view(output.size(0), -1)
+        fc = self.fc1(flatten)
+        out = F.log_softmax(fc, dim=1)
+        return fc
+
+
