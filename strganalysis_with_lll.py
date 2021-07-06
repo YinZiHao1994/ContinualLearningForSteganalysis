8b750c6c6ba0b7a504c4e1a4798dc723db438cb9
Yin <1035909389@qq.com>
2021-07-06 14:59:29 +0800

    删除无用的代码

--- strganalysis_with_lll.py
+++ strganalysis_with_lll.py
@@ -27,20 +27,17 @@ from MAS import mas
 from MAS.masUtils import utils, model_utils
 
 BATCH_SIZE = 32
-EPOCHS = 100
-LR = 0.01
-WEIGHT_DECAY = 5e-4
-TRAIN_PRINT_FREQUENCY = 50
-EVAL_PRINT_FREQUENCY = 1
-STETSIZE = 14
-scheduler_gama = 0.40
 # DECAY_EPOCH = [30, 60, 90, 140, 200, 250, 300, 350]
 # DECAY_EPOCH = [20, 60, 90, 120, 150, 170, 190]
 DECAY_EPOCH = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190]
 LOG_PATH = 'data/log'
 DATASET_DIR = r'D:\Work\dataset\steganalysis\BOSSBase'
 use_gpu = torch.cuda.is_available()
-reuse_model = True
+
+num_epochs = 100
+num_freeze_layers = 0
+lr = 0.001
+reg_lambda = 1
 
 train_dset_loaders = []
 valid_dset_loaders = []
@@ -51,61 +48,6 @@ class SteganographyEnum(Enum):
     HILL = 1
     SUNI = 2
     UTGAN = 3
-
-
-def evaluate(model, device, data_loader, epoch):
-    logging.info('start evaluate')
-    model.eval()
-    test_loss = 0.0
-    correct = 0.0
-    total = 0
-    batch_num = 0
-    best_acc = 0.0
-
-    with torch.no_grad():
-        for i, sample in enumerate(data_loader):
-            if i % 10 == 0:
-                print("evaluate in {}/{}".format(i, len(data_loader)))
-            data, label = sample['data'], sample['label']
-            shape = list(data.size())
-            data = data.reshape(shape[0] * shape[1], *shape[2:])
-            label = label.reshape(-1)
-            # shuffle
-            idx = torch.randperm(shape[0])
-            # data = data[idx]
-            # label = label[idx]
-
-            data, label = data.to(device), label.to(device)
-            output = model(data)
-            criterion = nn.CrossEntropyLoss()
-            loss = criterion(output, label)
-            test_loss += loss.item()
-            batch_num += 1
-
-            pred = output.max(1, keepdim=True)[1]
-            total += label.size(0)
-            correct += pred.eq(label.view_as(pred)).sum().item()
-
-    # accuracy = correct / (len(eval_loader.dataset) * 2)
-    accuracy = 100. * correct / total
-
-    if accuracy > best_acc:
-        best_acc = accuracy
-        # all_state = {
-        #     'original_state': model.state_dict(),
-        #     'optimizer_state': optimizer.state_dict(),
-        #     'epoch': epoch
-        # }
-        # torch.save(all_state, params_path)
-
-    logging.info('-' * 8)
-    test_loss = test_loss / batch_num
-    logging.info('Test in epoch {}'.format(epoch))
-    logging.info('Test loss: {:.4f}'.format(test_loss))
-    logging.info('Eval accuracy: {:.4f}'.format(accuracy))
-    logging.info('Best accuracy:{:.4f}'.format(best_acc))
-    logging.info('-' * 8)
-    return best_acc, test_loss
 
 
 def set_logger(log_path, mode='a'):
@@ -122,74 +64,6 @@ def set_logger(log_path, mode='a'):
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(logging.Formatter('%(message)s'))
         logger.addHandler(stream_handler)
-
-
-def train_implement(model, device, train_loader, optimizer, epoch):
-    # losses = steganalysis_utils.AverageMeter()
-    model.train()
-    train_loss = 0
-    train_correct = 0
-    total = 0
-
-    for i, sample in enumerate(train_loader):
-
-        datas, labels = sample['data'], sample['label']
-        shape = list(datas.size())
-        datas = datas.reshape(shape[0] * shape[1], *shape[2:])
-        labels = labels.reshape(-1)
-        # shuffle
-        idx = torch.randperm(shape[0])
-        data = datas[idx]
-        label = labels[idx]
-
-        data, label = data.to(device), label.to(device)
-
-        optimizer.zero_grad()
-        output = model(data)  # FP
-        criterion = nn.CrossEntropyLoss()
-        loss = criterion(output, label)
-        # losses.update(loss.item(), data.size(0))
-        loss.backward()  # BP
-        optimizer.step()
-        train_loss += loss.item()
-        prediction = torch.max(output, 1)  # second param "1" represents the dimension to be reduced
-        total += label.size(0)
-        train_correct += np.sum(prediction[1].cpu().numpy() == label.cpu().numpy())
-
-        if i % TRAIN_PRINT_FREQUENCY == 0:
-            logging.info('train epoch: [{0}][{1}/{2}]\t'
-                         'Acc {acc:.4f}\t'
-                         'Loss {loss:.4f} \t'
-                         .format(epoch, i, len(train_loader), acc=100. * train_correct / total,
-                                 loss=train_loss / (i + 1)))
-    return model
-
-
-def train_model(device, model, params_save_file_path, train_loader, valid_loader):
-    params = model.parameters()
-    params_wd, params_rest = [], []
-    for param_item in params:
-        if param_item.requires_grad:
-            (params_wd if param_item.dim() != 1 else params_rest).append(param_item)
-    param_groups = [{'params': params_wd, 'weight_decay': WEIGHT_DECAY},
-                    {'params': params_rest}]
-    optimizer = optim.SGD(param_groups, lr=LR, momentum=0.9, weight_decay=0.0005)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=STETSIZE, gamma=scheduler_gama)
-
-    for epoch in range(1, EPOCHS + 1):
-        # scheduler.step()
-        model = train_implement(model, device, train_loader, optimizer, epoch)
-        if epoch % EVAL_PRINT_FREQUENCY == 0 or epoch == EPOCHS:
-            evaluate(model, device, valid_loader, epoch)
-        print('current lr: ', optimizer.state_dict()['param_groups'][0]['lr'])
-        scheduler.step()
-
-    all_state = {
-        'original_state': model.state_dict(),
-        'optimizer_state': optimizer.state_dict(),
-    }
-    torch.save(all_state, params_save_file_path)
-    return model
 
 
 def init_logger(steganography_enum):
@@ -237,12 +111,6 @@ def generate_data_loaders(steganography_enum):
     valid_loader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=valid_sampler, )
     test_loader = DataLoader(dataset, batch_size=BATCH_SIZE, sampler=test_sampler, )
     return test_loader, train_loader, valid_loader
-
-
-num_epochs = 50
-num_freeze_layers = 0
-lr = 0.001
-reg_lambda = 1
 
 
 def main(steganography_enums):
